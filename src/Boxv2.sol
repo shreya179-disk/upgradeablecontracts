0e5ee656d3256a8e081042325789cf64aeead02f
shreya179-disk <shreyaprabhakar17@gmail.com>
2024-08-24 20:49:23 +0530

    first commit

---
+++ src/Boxv2.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from"@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract Boxv2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+  uint256 public num;
+  uint internal favnum;
+
+  // @custom:oz-upgrades-unsafe-allow constructor
+  constructor() {
+        _disableInitializers();
+  }
+
+  function initialize(address initialOwner)
+      initializer public {
+        __Ownable_init(initialOwner);
+        __UUPSUpgradeable_init();
+       
+  }
+   
+  function setNum(uint256 _num) external{
+      num  = _num;
+  }
+  function getVersion() external pure returns(uint){
+     return 20;
+  }
+  function getfavnum(uint _favnum) external {
+   
+  }
+  function _authorizeUpgrade(address newImplementation) internal override onlyOwner {
+        // Authorization logic for upgrades
+  }
+  
+}
